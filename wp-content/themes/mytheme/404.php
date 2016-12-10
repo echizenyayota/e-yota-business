b9587c81fc8f6bd3284ae4699c9476775d243c86
echizenyayota <virtualmisso30@gmail.com>
2016-12-10 20:54:11 +0900

    404.phpページの追加（オリジナル)

---
+++ wp-content/themes/mytheme/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<div class="container">
+  <div class="contents">
+    <h2>404 not found!</h2>
+      <p>申し訳ありません、お探しの記事はございません。</p>
+      <p><a href="<?php echo home_url(); ?>">トップページへ戻る</a></p>
+  </div>
+  <div class="sub">
+    <?php get_sidebar(); ?>
+  </div>
+</div>
+<?php get_footer(); ?>
